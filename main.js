49947450de71bb39af31e625aca3a04a9f03d6da
vivaan605 <81458609+vivaan605@users.noreply.github.com>
2022-05-24 15:16:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function setup()
+{
+canvas = createCanvas(400,400);
+video = createCapture(VIDEO);
+video.hide();
+}
+
+function draw(){
+    image(video,0,0,400,400);
+    check_sketch();
+    if(drawn_sketch == sketch){
+        answer_holder = "set";
+        score = score+1;
+        document.getElementById("score").innerHTML = "Score: "+score;
+    }
+}
+
+function check_sketch(){
+    timer_counter++;
+    document.getElementById("timer").innerHTML = "Timer: "+timer_counter;
+    if(timer_counter>500){
+        timer_counter = 0;
+        timer_check = "completed";
+    }
+    if(timer_check == "completed" || answer_holder == "set"){
+        timer_check = "";
+        answer_holder = "";
+        updateCanvas();
+    }
+}
